cb64093226bfcd4f436003bce6ad7696bab356c3
KukuaP <kpaintsil2ggc.edu>
2025-11-13 15:41:20 -0500

    changes

--- src/View/View.java
+++ src/View/View.java
@@ -1,12 +1,23 @@
+//Handles all the printout messages
 package View;
-import java.util.*;
 import Model.*;
 
+import java.util.Map;
+
 public class View {
-    public void helpCommand(Iterable<Commands> entries) {
-        System.out.println("Commands List: ");
-        for (Commands c : entries) {
-            System.out.println(c.cmdName() + ": " + c.hashCode()mdDesc());
+    public void printHelp(Iterable<Commands> entries){
+        System.out.println("In game commands: ");
+        for (Commands cmd : entries) {
+            System.out.println(cmd.getName() + ": " + cmd.getDescription());
+        }
+    }
+
+    public void printExits(Room rm) {
+        Map<String, String> exits = rm.getExits();
+        if (exits == null || exits.isEmpty()) {
+            System.out.println("This room has no exits.");
+        } else {
+            System.out.println("Exits out of this room are: <" + exits + ">");
         }
     }
 }
