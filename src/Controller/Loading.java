30d9f24931534810f64694a9da58d3d70983d0c4
nrivas1 <nrivas1@ggc.edu>
2025-11-15 12:50:05 -0500

    updated Loading.java

--- src/Controller/Loading.java
+++ src/Controller/Loading.java
@@ -7,6 +7,22 @@ import java.util.*;
 public class Loading {
     /* -------------------- Loading Rooms-------------------- */
     public void loadRooms(String filePath) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            int lineNo = 0;
+
+            while ((line = br.readLine()) != null) {
+                lineNo++;
+                line = line;
+                if (line.isEmpty() || line.startsWith("#")) continue;
+
+                String[] tokens = line.split(",");
+
+                if (tokens.length < 3) {
+                    throw new IllegalStateException("Line " + lineNo + " has fewer than 3 fields.");
+                }
+            }
+        }
 
     }
 
